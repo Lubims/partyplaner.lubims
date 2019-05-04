c84549eeffad1a22ec2efe7c96cb8b42f36ac663
robin2810 <robinbehrendt@web.de>
2019-05-04 15:08:57 +0200

    Update footer, added impressum
    ~ footer -> link zu impressum
    + impressum.php -> Impressum
    + impressum.jpg -> hintergrundbild

---
+++ project-barney/src/impressum.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Impressum</title>
+    <style>
+      html, body {
+        height: 100%;
+      }
+      body {
+        overflow:hidden;
+        background-image: url("/php-2019/project-barney/lib/impressum.jpg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        text-align: center;
+        text-shadow:
+      		-0.5px -0.5px 0 white,
+      		0.5px -0.5px 0 white,
+      		-0.5px 0.5px 0 white,
+      		0.5px 0.5px 0 white;
+        color: purple;
+        font-family: "Verdana";
+        font-size: 14px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>&copy; LUBAR - EINE NIEDERLASSUNG DER LUBIMS AG</h1>
+    <h4 style="transform: translate(-570px, 80px)">
+      Ansässig in der:<br>
+      Werner-Peix-Straße 29<br>
+      31789 Hameln
+    </h4>
+    <h4 style="transform: translate(130px, 85px)">
+      Zweigstelle:<br>
+      Weibecker Straße 16<br>
+      31840 Hessisch Oldendorf
+    </h4>
+  </body>
+  <span style="position: fixed; bottom: 0pt; transform: translate(-50px)">
+    <p><a style="text-align: center; color: black; font-size: 12px" href="/index.php">Zurück zur Homepage</a></p>
+  </span>
+<html>
