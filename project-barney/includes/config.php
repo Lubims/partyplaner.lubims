daff1ad2724ae7bde628527e06901ad0c6060d4e
NikIsImba <nik123691@gmail.com>
2019-05-15 11:52:17 +0200

    Seiten sicher gemacht

--- project-barney/includes/config.php
+++ project-barney/includes/config.php
@@ -7,7 +7,7 @@ if(session_id() == ''){
 switch ($_SERVER["SCRIPT_NAME"]) {
   case    "/php-2019/project-barney/src/dashboard/profil.php":
           $CURRENT_PAGE = "Profil";
-          $PAGE_TITLE = "Das ist das Profil";
+          $PAGE_TITLE = "Profil";
           if (!isset($_SESSION['user'])){
             header("Location: /php-2019/project-barney/");
             exit;
@@ -15,7 +15,39 @@ switch ($_SERVER["SCRIPT_NAME"]) {
           break;
   case    "/php-2019/project-barney/src/index_log.php":
           $CURRENT_PAGE = "IndexLog";
-          $PAGE_TITLE = "Index aber jetzt eingelogt";
+          $PAGE_TITLE = "Projekt Barney";
+          if (!isset($_SESSION['user'])){
+            header("Location: /php-2019/project-barney/");
+            exit;
+          }
+          break;
+  case    "/php-2019/project-barney/src/dashboard/projekte.php":
+          $CURRENT_PAGE = "Projekte";
+          $PAGE_TITLE = "Projekte";
+          if (!isset($_SESSION['user'])){
+            header("Location: /php-2019/project-barney/");
+            exit;
+          }
+          break;
+  case    "/php-2019/project-barney/src/dashboard/neue_projekte.php":
+          $CURRENT_PAGE = "NeueProjekte";
+          $PAGE_TITLE = "Neue Projekte";
+          if (!isset($_SESSION['user'])){
+            header("Location: /php-2019/project-barney/");
+            exit;
+          }
+          break;
+  case    "/php-2019/project-barney/src/freunde.php":
+          $CURRENT_PAGE = "Freunde";
+          $PAGE_TITLE = "Freunde";
+          if (!isset($_SESSION['user'])){
+            header("Location: /php-2019/project-barney/");
+            exit;
+          }
+          break;
+  case    "/php-2019/project-barney/src/profil_bearbeiten.php":
+          $CURRENT_PAGE = "ProfilBearbeiten";
+          $PAGE_TITLE = "Profil Bearbeiten";
           if (!isset($_SESSION['user'])){
             header("Location: /php-2019/project-barney/");
             exit;
