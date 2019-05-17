7e693a9dadfe4718e9ffe43fe10a4c423d5299da
robin2810 <robinbehrendt@web.de>
2019-05-17 11:54:29 +0200

    Update test.php

--- project-barney/test.php
+++ project-barney/test.php
@@ -14,7 +14,7 @@ $code = mt_rand(100000, 999999);
 $signup_username = "robin2810";
 $signup_email = "robinbehrendt@web.de";
 $signup_pwd = "123";
-$dsn = "mysql:81.169.248.236; dbname=alkdb";
+$dsn = "mysql:h2836116.stratoserver.net; dbname=alkdb";
 $user = "root";
 $password = "php-2019";
 
