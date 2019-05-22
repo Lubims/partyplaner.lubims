23538dec68dffe1f2b068535c719f5b8c9f19021
robin2810 <robinbehrendt@web.de>
2019-05-22 11:56:26 +0200

    sql lowercase
    spalten/tabellennamen zu lowercase wegen linux.
    alte datei _old falls fehler.
    
    email VARCHAR von 20 auf 50

--- sql/CREATE_TABLE.sql
+++ sql/CREATE_TABLE.sql
@@ -1,59 +1,59 @@
-CREATE DATABASE  AlkDB;
+CREATE DATABASE  alkdb;
 
-CREATE TABLE Produkte (
-	ProduktID INT NOT NULL AUTO_INCREMENT,
-	Name VARCHAR(20) NOT NULL,
-	Firma VARCHAR(30) NOT NULL,
-	Beschreibung VARCHAR(500),
-	AlkWert DECIMAL(3,1) NOT NULL,
+CREATE TABLE produkte (
+	produktid INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	firma VARCHAR(30) NOT NULL,
+	beschreibung VARCHAR(500),
+	alkwert DECIMAL(3,1) NOT NULL,
 
-	PRIMARY KEY (ProduktID)
+	PRIMARY KEY (produktid)
 );
 
-CREATE TABLE Benutzer (
-	UserID INT NOT NULL AUTO_INCREMENT,
-	Username VARCHAR(20) NOT NULL,
-	Email VARCHAR(20) NOT NULL,
-	Passwort VARCHAR(255) NOT NULL,
-	Code VARCHAR(6),
+CREATE TABLE benutzer (
+	userid INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(20) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	passwort VARCHAR(255) NOT NULL,
+	code VARCHAR(6),
 
-	PRIMARY KEY (UserID)
+	PRIMARY KEY (userid)
 );
 
-CREATE TABLE Freunde (
-	User1ID INT NOT NULL,
-	User2ID INT NOT NULL,
+CREATE TABLE freunde (
+	user1id INT NOT NULL,
+	user2id INT NOT NULL,
 
-	PRIMARY KEY (User1ID, User2ID),
-	FOREIGN KEY (User1ID) REFERENCES Benutzer(UserID),
-	FOREIGN KEY (User2ID) REFERENCES Benutzer(UserID)
+	PRIMARY KEY (user1id, user2id),
+	FOREIGN KEY (user1id) REFERENCES benutzer(userid),
+	FOREIGN KEY (user2id) REFERENCES benutzer(userid)
 );
 
-CREATE TABLE Projekte (
-	ProjektID INT NOT NULL AUTO_INCREMENT,
-	ProjektName VARCHAR(30) NOT NULL,
-	Termin DATE NOT NULL,
-	Zeit VARCHAR(30) NOT NULL,
-	Beschreibung VARCHAR(500),
+CREATE TABLE projekte (
+	projektid INT NOT NULL AUTO_INCREMENT,
+	projektname VARCHAR(30) NOT NULL,
+	termin DATE NOT NULL,
+	zeit VARCHAR(30) NOT NULL,
+	beschreibung VARCHAR(500),
 
-	PRIMARY KEY (ProjektID)
+	PRIMARY KEY (projektid)
 );
 
-CREATE TABLE ProjektUser(
-	ProjektID INT NOT NULL,
-	UserID INT NOT NULL,
-	Besitzer BOOLEAN,
+CREATE TABLE projektuser(
+	projektid INT NOT NULL,
+	userid INT NOT NULL,
+	besitzer BOOLEAN,
 
-	PRIMARY KEY (ProjektID, UserID),
-	FOREIGN KEY (ProjektID) REFERENCES Projekte(ProjektID),
-	FOREIGN KEY (UserID) REFERENCES Benutzer(UserID)
+	PRIMARY KEY (projektid, userid),
+	FOREIGN KEY (projektid) REFERENCES projekte(projektid),
+	FOREIGN KEY (userid) REFERENCES benutzer(userid)
 );
 
-CREATE TABLE Produktliste(
-ProjekteID INT NOT NULL,
-ProduktID INT NOT NULL,
+CREATE TABLE produktliste(
+projekteid INT NOT NULL,
+produktid INT NOT NULL,
 
-PRIMARY KEY (ProjekteID, ProduktID),
-FOREIGN KEY (ProduktID) REFERENCES Produkte(ProduktID),
-FOREIGN KEY (ProjekteID) REFERENCES Projekte(ProjektID)
+PRIMARY KEY (projekteid, produktid),
+FOREIGN KEY (produktid) REFERENCES produkte(produktid),
+FOREIGN KEY (projekteid) REFERENCES projekte(projektid)
 );
