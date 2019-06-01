b9083bb129596f47e97afcc18f9b0dd3bbe2626f
Niklas <nik123691@gmail.com>
2019-06-01 21:34:46 +0200

    Update projekte_set_daten.php

--- project-barney/src/dashboard/projekte_set_daten.php
+++ project-barney/src/dashboard/projekte_set_daten.php
@@ -14,7 +14,7 @@ try {
     $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     //Testen ob es den nutzer schon gibt
     $selectStmt = $dbh->prepare("UPDATE projekte SET projektname = ?, termin = ?, zeit = ?, ort = ?, beschreibung = ? WHERE projektid = ?");
-    $selectStmt->execute([$_POST['veranstaltungsname_neu'], $_POST['termin_neu'], $_POST['uhrzeit_neu'], $_POST['ort_neu'], $_POST['beschreibung_neu'], $_POST['projektid']]);
+    $selectStmt->execute([htmlspecialchars($_POST['veranstaltungsname_neu']), htmlspecialchars($_POST['termin_neu']), htmlspecialchars($_POST['uhrzeit_neu']), htmlspecialchars($_POST['ort_neu']), htmlspecialchars($_POST['beschreibung_neu']), htmlspecialchars($_POST['projektid'])]);
 
     header("Location: projekte.php");
     die();
