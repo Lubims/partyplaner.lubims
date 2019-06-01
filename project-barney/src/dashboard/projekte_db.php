994eecd4cf1f6cb11d606b0860e0a3f0caa99b95
Niklas <nik123691@gmail.com>
2019-06-01 18:55:55 +0200

    Update projekte_db.php

--- project-barney/src/dashboard/projekte_db.php
+++ project-barney/src/dashboard/projekte_db.php
@@ -57,3 +57,70 @@ try {
     echo '0 results';
     }
     ?>
+
+      <thead>
+        <tr>
+          <th>Alte Projekte</th>
+        </tr>
+      </thead>
+
+
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Termin</th>
+          <th>Uhrzeit</th>
+          <th>Ort</th>
+          <th>Beschreibung</th>
+          <th>Änderungen</th>
+        </tr>
+      </thead>
+      <?php
+      try {
+          $dbh = new PDO($dsn, $user, $password);
+          $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+          //Testen ob es den nutzer schon gibt
+          $selectStmt = $dbh->prepare("SELECT * FROM projekte WHERE projektid IN (SELECT projektid FROM projektuser WHERE userid = (SELECT userid FROM benutzer WHERE username = :username))AND termin < CURDATE() ORDER BY termin DESC");
+          $selectStmt->bindParam(":username", $_SESSION['user'], PDO::PARAM_STR, 12);
+          $selectStmt->execute();
+
+          $projekte = $selectStmt->fetchAll();
+
+      } catch (PDOException $e) {
+          echo 'Connection failed: ' . $e->getMessage();
+          die();
+      }
+
+          if ($projekte) {
+          // output data of each row
+          foreach ($projekte as $row => $link) { ?>
+          <tbody>
+          <tr>
+          <th>
+          <?php echo $link['projektid']; ?>
+          </th>
+          <td>
+          <?php echo $link['projektname']; ?>
+          </td>
+          <td>
+          <?php echo $link['termin']; ?>
+          </td>
+          <td>
+          <?php echo $link['zeit']; ?>
+          </td>
+          <td>
+          <?php echo $link['ort']; ?>
+          </td>
+          <td>
+          <?php echo $link['beschreibung']; ?>
+          </td>
+          <td style="padding-left: 10px"><label for="modal-switch" class="btn btn-outline-success my-2 my-sm-0" role="button" data-toggle="modal" onClick="getProjektdaten(<?php echo $link['projektid']; ?>)">ändern</label></td>
+          </tr>
+          </tbody>
+          <?php
+          }
+          } else {
+          echo '0 results';
+          }
+          ?>
+    </table>
