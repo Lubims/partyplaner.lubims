d0a37e97eb7cce73f0e6b097bb7aa7f7b181f0de
Niklas <nik123691@gmail.com>
2019-06-02 21:07:27 +0200

    löschen

--- project-barney/src/profil_bearbeiten/account_loeschen.php
+++ project-barney/src/profil_bearbeiten/account_loeschen.php
@@ -7,7 +7,21 @@ $password = "";
 try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    //Testen ob es den nutzer schon gibt
+
+    $Stmt = $dbh->prepare("SELECT userid FROM benutzer WHERE username = :username");
+    $Stmt->bindParam(":username", $_SESSION['user'], PDO::PARAM_STR, 12);
+    $Stmt->execute();
+    $userid = $Stmt->fetch();
+
+    $Stmt = $dbh->prepare("DELETE FROM produktliste WHERE userid = ?");
+    $Stmt->execute([$userid['userid']]);
+
+    $Stmt = $dbh->prepare("DELETE FROM freunde WHERE user1id = ? OR user2id = ?");
+    $Stmt->execute([$userid['userid'], $userid['userid']]);
+
+    $Stmt = $dbh->prepare("DELETE FROM projektuser WHERE userid = ?");
+    $Stmt->execute([$userid['userid']]);
+
     $Stmt = $dbh->prepare("DELETE FROM benutzer WHERE Username = ?");
     $Stmt->execute([$_SESSION['user']]);
 
