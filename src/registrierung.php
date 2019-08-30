3b1f8fe15e1351cf5cdbacd65961c9425fdfda05
robin2810 <robinbehrendt@web.de>
2019-08-30 13:30:23 +0200

    Update registrierung.php

--- src/registrierung.php
+++ src/registrierung.php
@@ -18,12 +18,9 @@ require dirname(__DIR__).'/lib/PHPMailer/src/SMTP.php';
 
 //Variablen
 $code = mt_rand(100000, 999999);
-//$signup_email = htmlspecialchars($_POST["signup_email"]);
-//$signup_pwd = htmlspecialchars($_POST["signup_pwd"]);
-//$signup_username = htmlspecialchars($_POST["signup_username"]);
-$signup_email = "admin@lubims.de";
-$signup_pwd = "test";
-$signup_username = "admin";
+$signup_email = htmlspecialchars($_POST["signup_email"]);
+$signup_pwd = htmlspecialchars($_POST["signup_pwd"]);
+$signup_username = htmlspecialchars($_POST["signup_username"]);
 $dsn = "mysql:host=localhost:3306;dbname=kd58916_alkdb";
 $user = "kd58916_root";
 $password = "At452B7L9s";
